b3a7934edbbd9abf2907f4b831678d37ad26b963
Adi Yulianto <adiyulianto888@gmail.com>
2022-01-17 01:21:10 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dicoding-submission-forum-api
+ Dicoding - Submission Menjadi Back-End Developer Expert : Forum API
---
+++ migrations/1635222174820_create-table-comments.js
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable("comments", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    content: {
+      type: "TEXT",
+      notNull: true,
+    },
+    owner: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    thread: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    created_at: {
+      type: "TEXT",
+      notNull: true,
+    },
+    updated_at: {
+      type: "TEXT",
+      notNull: true,
+    },
+    is_delete: {
+      type: "BOOLEAN",
+      default: false,
+      notNull: false,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("comments");
+};
